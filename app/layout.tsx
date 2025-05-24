58a500008ac94b4da4efeba9b650766e6ab21627
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-24 20:18:53 +0800

    init commite

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <header><h1>This is the header</h1></header>
+      <body className={`${inter.className} antialiased`}>{children}</body>
+      <footer><h1>This is the footer</h1></footer>
     </html>
   );
 }
